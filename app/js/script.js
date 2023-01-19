833f17350211055330bdebbb25339211f7b8d2ea
Ukeh Hyginus <hyginusukeh6@gmail.com>
2023-01-19 16:02:50 +0100

    header html and css

--- app/js/script.js
+++ app/js/script.js
@@ -1,1 +1,11 @@
-console.log("hello world");
+const btnMenu = document.querySelector('#btn_menu');
+btnMenu.addEventListener('click', function(){
+    console.log("open menu");
+
+    if(btnMenu.classList.contains('open')){
+        btnMenu.classList.remove('open');
+    }else{
+        btnMenu.classList.add('open');
+    }
+    
+});

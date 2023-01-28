bd81a257ae9072f9e940c508b12a7c3223c20e61
Ukeh Hyginus <hyginusukeh6@gmail.com>
2023-01-28 22:21:14 +0100

    Update on FAQ

--- app/js/script.js
+++ app/js/script.js
@@ -1,6 +1,6 @@
 const btnMenu = document.querySelector('#btn_menu');
 btnMenu.addEventListener('click', function(){
-    console.log("open menu");
+    // console.log("open menu");
 
     if(btnMenu.classList.contains('open')){
         btnMenu.classList.remove('open');
@@ -8,4 +8,20 @@ btnMenu.addEventListener('click', function(){
         btnMenu.classList.add('open');
     }
     
+});
+
+const faqText = document.querySelector('.faq__text');
+const faqClose = document.querySelector('.faq__close');
+const faqOpen = document.querySelector('.faq__open');
+
+faqClose.addEventListener('click',function(){
+    faqText.style.display = "none";
+    faqOpen.style.display = "block";
+    faqClose.style.display = "none";
+});
+
+faqOpen.addEventListener('click',function(){
+    faqText.style.display = "block";
+    faqOpen.style.display = "none";
+    faqClose.style.display = "block";
 });
